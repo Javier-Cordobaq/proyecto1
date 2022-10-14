533e8acab79233f8280927b5578747919d90a513
Javier Córdoba <80362946+Javier-Cordobaq@users.noreply.github.com>
2022-10-14 15:04:49 -0500

    images-producst-home
    Se agrego un fondo de burbuja a la imágenes de los productos en la sección de productos en el home

--- client/src/components/SeccionProductos/SeccionProductos.jsx
+++ client/src/components/SeccionProductos/SeccionProductos.jsx
@@ -10,42 +10,42 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import productosTitulo from '../../Imagenes/productosTitulo.png'
 import popsTitulo from '../../Imagenes/popsTitulo.png'
-export default function SeccionProductos(){
+export default function SeccionProductos() {
     const idioma = useSelector(state => state.idioma)
     const arraySnacks = [
         {
-            img:snack1,
-            nombre:'Spicy Cheese',
+            img: snack1,
+            nombre: 'Spicy Cheese',
         },
         {
-            img:snack2,
-            nombre:'Sea Salt',
+            img: snack2,
+            nombre: 'Sea Salt',
         },
         {
-            img:snack3,
-            nombre:'Sweet & Salty',
+            img: snack3,
+            nombre: 'Sweet & Salty',
         },
     ]
 
     const rings = [
         {
-            img:r1,
-            nombre:'White Cheddar',
+            img: r1,
+            nombre: 'White Cheddar',
         },
         {
-            img:r2,
-            nombre:'Caramel Churro',
+            img: r2,
+            nombre: 'Caramel Churro',
         },
         {
-            img:r3,
-            nombre:'Sour cream & Onion',
+            img: r3,
+            nombre: 'Sour cream & Onion',
         }
     ]
 
-    return(
+    return (
         <div id='productos' className={style.globaContainer}>
             <div className={style.title}>
-             <img src={productosTitulo} alt="" />
+                <img src={productosTitulo} alt="" />
             </div>
             <div className={style.container}>
                 <img src={popsTitulo} className={style.imgTitulo} alt="" />
@@ -54,52 +54,56 @@ export default function SeccionProductos(){
                         arraySnacks.map(snack => (
 
                             <div className={style.producto}>
-                                <img className={style.img} src={snack.img} alt=""/>
+                                <div className={style.container_img}>
+                                    <img className={style.img} src={snack.img} alt="" />
+                                </div>
                                 <div className={style.comprar}>
                                     <div className={style.botton}>
                                         <p>{snack.nombre}</p>
-                                        <Link to='/tienda'><button>{idioma === "español" ?"Comprar":"Buy"}</button></Link>
+                                        <Link to='/tienda'><button>{idioma === "español" ? "Comprar" : "Buy"}</button></Link>
                                     </div>
                                     <div className={style.tablanutriocional}>
                                         <p>{idioma === "español" ? "Ver tabla nutricional" : "Show nutritional table +1"}</p>
                                     </div>
                                 </div>
-                                
+
 
                             </div>
                         ))
-                    }   
-                    
+                    }
+
                 </div>
             </div>
 
             <div className={style.container}>
                 <img src={popsTitulo} className={style.imgTitulo} alt="" />
                 <div className={style.gridProductos}>
-                {
-                rings.map(snack => (
+                    {
+                        rings.map(snack => (
 
-                    <div className={style.producto}>
-                        <img className={style.img} src={snack.img} alt=""/>
-                        <div className={style.comprar}>
-                            <div className={style.botton}>
-                                <p className={style.name}>{snack.nombre}</p>
-                                <Link to='/tienda'><button>{idioma === "español" ?"Comprar":"Buy"}</button></Link>
-                            </div>
-                            <div className={style.tablanutriocional}>
-                            <p>{idioma === "español" ? "Ver tabla nutricional" : "Show nutritional table +1"}</p>
-                            </div>
-                        </div>
-                        
+                            <div className={style.producto}>
+                                <div className={style.container_img}>
+                                    <img className={style.img} src={snack.img} alt="" />
+                                </div>
+                                <div className={style.comprar}>
+                                    <div className={style.botton}>
+                                        <p className={style.name}>{snack.nombre}</p>
+                                        <Link to='/tienda'><button>{idioma === "español" ? "Comprar" : "Buy"}</button></Link>
+                                    </div>
+                                    <div className={style.tablanutriocional}>
+                                        <p>{idioma === "español" ? "Ver tabla nutricional" : "Show nutritional table +1"}</p>
+                                    </div>
+                                </div>
 
-                    </div>
-                ))
-            }   
-                    
+
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
         </div>
 
-        
+
     )
 }
